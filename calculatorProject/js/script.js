9d32766e7702d558e862e7b968e3273e4d525b1c
Txell Amigó <txellmgsol@gmail.com>
2023-04-23 17:56:10 +0200

    Add range function, not working yet

--- calculatorProject/js/script.js
+++ calculatorProject/js/script.js
@@ -5,7 +5,8 @@ var num2 = +prompt("Give me number 2");
 // Validation: numbers requested in range 1-50
 function numbersValidation() {
   if (num1 > 50 || num2 > 50 || num1 < 1 || num2 < 1) {
-    alert(
+    //alert("The numbers requested need to be positive integers between 1 and 50!");
+    console.log(
       "The numbers requested need to be positive integers between 1 and 50!"
     );
   }
@@ -15,19 +16,23 @@ function numbersValidation() {
 function sumNum1Num2(num1, num2) {
   let sum = num1 + num2;
   console.log("The sum is: " + sum);
-  //return alert('The sum is: ' + sum);
+  //alert('The sum is: ' + sum);
 }
 
 // Prime function
 function primeNum1Num2(num1, num2) {
   if ((num1 % num1 == 0) & (num2 % num2 == 0)) {
     console.log(num1 + " and " + num2 + " ara prime numbers");
+    //alert(num1 + " and " + num2 + " ara prime numbers");
   } else if ((num1 % num1 == 0) & (num2 % num2 != 0)) {
     console.log("Only " + num1 + " is a prime number");
+    //alert("Only " + num1 + " is a prime number");
   } else if ((num1 % num1 != 0) & (num2 % num2 == 0)) {
     console.log("Only " + num2 + " is a prime number");
+    //alert("Only " + num2 + " is a prime number");
   } else {
     console.log("None of them ara prime numbers");
+    //alert("None of them ara prime numbers");
   }
 }
 
@@ -35,17 +40,44 @@ function primeNum1Num2(num1, num2) {
 function evenOddNum1Num2(num1, num2) {
   if ((num1 % 2 == 0) & (num2 % 2 == 0)) {
     console.log(num1 + " and " + num2 + " ara even numbers");
+    //alert(num1 + " and " + num2 + " ara even numbers");
   } else if ((num1 % 2 == 0) & (num2 % 2 != 0)) {
     console.log(num1 + " is an even number and " + num2 + " is an odd number");
+    //alert(num1 + " is an even number and " + num2 + " is an odd number");
   } else if ((num1 % 2 != 0) & (num2 % 2 == 0)) {
     console.log(num2 + " is an even number and " + num1 + " is an odd number");
+    //alert(num2 + " is an even number and " + num1 + " is an odd number");
   } else {
     console.log("Both " + num1 + "and " + num2 + " are odd numbers");
+    //alert("Both " + num1 + "and " + num2 + " are odd numbers");
   }
 }
 
-// Call functions
+// list numbers function
+function listNumbers(num1, num2) {
+  let arrayList = [];
+  if (num1 < num2) {
+    for (i = num1; i <= num2; i++) {
+      if (i % 2 == 0) {
+        arrayList.add(i);
+      }
+    }
+  } else if (num1 > num2) {
+    for (i = num1; i <= num2; i--) {
+      if (i % 2 != 0) {
+        arrayList.add(i);
+      }
+    }
+  } else {
+    arrayList.add(num1);
+  }
+  //alert('Range: ' + arrayList);
+  console.log("Range: " + arrayList);
+}
+
+// Call functions (calculation function)
 numbersValidation(num1, num2);
 sumNum1Num2(num1, num2);
 primeNum1Num2(num1, num2);
 evenOddNum1Num2(num1, num2);
+listNumbers(num1, num2);
