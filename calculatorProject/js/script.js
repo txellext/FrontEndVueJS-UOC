7fe1071d0d5f48288a0468300ff77f871c1de202
Txell Amigó <txellmgsol@gmail.com>
2023-04-23 14:32:53 +0200

    Add summ function working

--- calculatorProject/js/script.js
+++ calculatorProject/js/script.js
@@ -1,13 +1,20 @@
-var num1 = prompt("Give me number 1");
-var num2 = prompt("Give me number 2");
+var num1 = +prompt("Give me number 1");
+var num2 = +prompt("Give me number 2");
 
 // Validation: numbers requested in range 1-50
 function numbersValidation() {
-    if (num1 > 50 || num2 > 50 || num1 < 1 || num2 < 1) {
-        alert("The numbers requested need to be positive integers between 1 and 50!");
-      }
-};
+  if (num1 > 50 || num2 > 50 || num1 < 1 || num2 < 1) {
+    alert(
+      "The numbers requested need to be positive integers between 1 and 50!"
+    );
+  }
+}
 
+// Summ function
+function summNum1Num2(num1, num2) {
+  summ = num1 + num2;
+  console.log(summ);
+  //return summ;
+}
 
-
-
+summNum1Num2(num1, num2);
