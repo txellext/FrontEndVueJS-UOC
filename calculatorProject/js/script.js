8b2463d048668d470b4252fe11a0b0d154ab8c83
Txell Amigó <txellmgsol@gmail.com>
2023-05-05 10:00:56 +0200

    Add numberValidation() modification, primeNum1Num2 modification and bootstrap links

--- calculatorProject/js/script.js
+++ calculatorProject/js/script.js
@@ -1,23 +1,20 @@
-// Declare variables
-// var num1 = +prompt("Give me number 1");
-// var num2 = +prompt("Give me number 2");
-
 // Validation: numbers requested in range 1-50
-// function numbersValidation(num1, num2) {
-//   let result;
-//   let stop = 0;
-//   if (num1 > 50 || num2 > 50 || num1 < 1 || num2 < 1) {
-//     console.log(
-//       "The numbers requested need to be positive integers between 1 and 50!"
-//     );
-//     result =
-//       "The numbers requested need to be positive integers between 1 and 50!";
-//   } else {
-//     stop = 1;
-//     console.log("The numbers requested are correct" + stop);
-//   }
-//   return result, stop;
-// }
+function numbersValidation(num1, num2) {
+  let result = "";
+  let stop = false;
+  if (num1 > 50 || num2 > 50 || num1 < 1 || num2 < 1) {
+    //console.log("The numbers requested need to be positive integers between 1 and 50!" + "\n");
+    alert(
+      (result =
+        "The numbers requested need to be positive integers between 1 and 50!" +
+        "\n")
+    );
+  } else {
+    stop = true;
+    //console.log("The numbers requested are correct" + stop);
+  }
+  return { result, stop };
+}
 
 // Sum function
 function sumNum1Num2(num1, num2) {
@@ -29,13 +26,14 @@ function sumNum1Num2(num1, num2) {
 // Prime function
 function primeNum1Num2(num1, num2) {
   let result;
-  if ((num1 % num1 == 0) & (num2 % num2 == 0)) {
+  let i = 2;
+  if ((num1 % i == 0) & (num2 % i == 0)) {
     //console.log(num1 + " and " + num2 + " ara prime numbers");
     result = num1 + " and " + num2 + " ara prime numbers";
-  } else if ((num1 % num1 == 0) & (num2 % num2 != 0)) {
+  } else if ((num1 % i == 0) & (num2 % i != 0)) {
     //console.log("Only " + num1 + " is a prime number");
     result = "Only " + num1 + " is a prime number";
-  } else if ((num1 % num1 != 0) & (num2 % num2 == 0)) {
+  } else if ((num1 % i != 0) & (num2 % i == 0)) {
     //console.log("Only " + num2 + " is a prime number");
     result = "Only " + num2 + " is a prime number";
   } else {
@@ -59,7 +57,7 @@ function evenOddNum1Num2(num1, num2) {
     result = num2 + " is an even number and " + num1 + " is an odd number";
   } else {
     //console.log("Both " + num1 + "and " + num2 + " are odd numbers");
-    result = "Both " + num1 + "and " + num2 + " are odd numbers";
+    result = "Both " + num1 + " and " + num2 + " are odd numbers";
   }
   return result;
 }
@@ -86,29 +84,21 @@ function range(num1, num2) {
   //console.log("Range: " + arrayList);
 }
 
-// Call functions (calculation function with a while or do while using numbersValidation as a condition)
+// Call functions (calculation function with a while or do while using numbersValidation() as a condition)
 
 function calculation() {
   let num1;
   let num2;
-  let stop = 0;
-  let result = "";
 
   do {
     num1 = +prompt("Give me number 1");
     num2 = +prompt("Give me number 2");
-    if (num1 > 50 || num2 > 50 || num1 < 1 || num2 < 1) {
-      console.log(
-        "The numbers requested need to be positive integers between 1 and 50!"
-      );
-      alert(
-        "The numbers requested need to be positive integers between 1 and 50!"
-      );
-    } else {
-      stop = 1;
-      console.log("The numbers requested are correct ");
-    }
-  } while (stop == 0);
+
+    let validationOutput = numbersValidation(num1, num2);
+    //console.log(validationOutput, "validationOutput");
+    result = validationOutput.result;
+    stop = validationOutput.stop;
+  } while (!stop);
 
   result += sumNum1Num2(num1, num2) + "\n";
   result += primeNum1Num2(num1, num2) + "\n";
