807ab9845b409a2ce68c52d67a406231e195ac2d
Txell Amigó <txellmgsol@gmail.com>
2023-05-29 23:26:42 +0200

    Add styles header + buttons

--- pokedexProject/js/getRandomPokemon.js
+++ pokedexProject/js/getRandomPokemon.js
@@ -1,39 +1,45 @@
-async function getRandomPokemon() {
+function getRandomPokemon() {
   let randomNum = -1;
   let randomNumArray = [];
   let i = 0;
   const numMax = 10;
+  const output = document.querySelector("#cards");
+  output.innerHTML = "";
 
-  while (!randomNumArray.includes(randomNum) || i < 10) {
+  while (!randomNumArray.includes(randomNum) || i < numMax) {
     randomNum = Math.round(Math.random() * 1010);
-    let response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${randomNum}`
-    );
-    let data = await response.json();
-    // console.log(data.name);
-    // console.log(data.id);
-    // console.log(data.sprites.other.home.front_default);
 
-    const output = document.querySelector("#cards");
-    const template = document.getElementById("template");
-    const clonedTemplate = template.content.cloneNode(true);
-
-    let card = clonedTemplate.querySelector(".card");
-    let pokemonId = clonedTemplate.querySelector(".pokemonID");
-    let randomPokemon = clonedTemplate.querySelector(".pokemonName");
-    let pokemonImg = clonedTemplate.querySelector("img");
-
-    card.setAttribute("id", data.id);
-    pokemonId.textContent = `#${data.id}`;
-    randomPokemon.textContent = data.name;
-    pokemonImg.setAttribute("src", data.sprites.other.home.front_default);
-
-    output.appendChild(clonedTemplate);
+    fetchApi(randomNum);
 
     randomNumArray.push(randomNum);
+
     i++;
   }
+  //console.log(randomNumArray);
 }
-//console.log(randomNumArray);
 
-export default getRandomPokemon;
+async function fetchApi(randomNum) {
+  let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`);
+  let data = await response.json();
+  // console.log(data.name);
+  // console.log(data.id);
+  // console.log(data.sprites.other.home.front_default);
+
+  const output = document.querySelector("#cards");
+  const template = document.getElementById("template");
+  const clonedTemplate = template.content.cloneNode(true);
+
+  let card = clonedTemplate.querySelector(".card");
+  let pokemonId = clonedTemplate.querySelector(".pokemonID");
+  let randomPokemon = clonedTemplate.querySelector(".pokemonName");
+  let pokemonImg = clonedTemplate.querySelector("img");
+
+  card.setAttribute("id", data.id);
+  pokemonId.textContent = `#${data.id}`;
+  randomPokemon.textContent = data.name;
+  pokemonImg.setAttribute("src", data.sprites.other.home.front_default);
+
+  output.appendChild(clonedTemplate);
+}
+
+export { getRandomPokemon };
--- pokedexProject/js/main.js
+++ pokedexProject/js/main.js
@@ -1,5 +1,13 @@
-import getRandomPokemon from "./getRandomPokemon.js";
+import { getRandomPokemon } from "./getRandomPokemon.js";
 
 //console.log("main.js working");
 
 getRandomPokemon();
+
+function onClickCards() {
+  document
+    .getElementById("button-random-pokemons")
+    .addEventListener("click", getRandomPokemon);
+}
+
+onClickCards();
